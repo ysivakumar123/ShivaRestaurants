1d920bc9ff5b8e87a95b15eeee3f1d16c4d92227
ysivakumar123 <118169801+ysivakumar123@users.noreply.github.com>
2022-11-14 15:15:03 +0530

    Add files via upload

---
+++ ShivaRestaurants/chunks/about.php
@@ -0,0 +1,28 @@
+<section class="fabout">
+		<div class="section white center">
+		      <div class="row container">
+		        <h2 class="header">ABOUT US</h2>
+		        <p class="grey-text text-darken-3 lighten-3"><b>WHO WE ARE.</b><br>
+<p style="text-align:left;">
+There's nothing cookie-cutter about Pizza Hut. Not our pizzas. Not our people. And definitely not the way we live life. Around here, we don't settle for anything less than food we're proud to serve. And we don't just clock in. Not when we can also become our best, make friends, and have fun while we're at it. We're the pizza company that lives life unboxed.
+
+We're not for people who want to blend in: pushing boundaries is part of our heritage. We have more than 16,000 restaurants and 350,000 team members in more than 100 countries. Whether it's the original Stuffed Crust or putting a pizza in outer space, we never stop driving ourselves to deliver hot pizzas, fast every time - anywhere you want to enjoy it.
+</p>
+<br>
+<b> WHAT WE'RE ABOUT.</b>
+<br>
+<p style="text-align:left;">
+At Pizza Hut, we don't just make pizza. We make people happy. Pizza Hut was built on the belief that pizza night should be special, and we carry that belief into everything we do. With more than 55 years of experience under our belts, we understand how to best serve our customers through tried and true service principles: We create food we're proud to serve and deliver it fast, with a smile.
+</p>
+<br>
+<b>WHERE WE COME FROM.</b>
+<br>
+<p style="text-align:left;">
+In 1958, two brothers borrowed $600 from their mom to open a pizza place in Wichita, Kansas. They named it Pizza Hut, because their sign only had room for eight letters. How profound!
+
+Soon, the restaurant grew. Why? The pizza was awesome. The service felt like home. And the customers were treated like family.
+
+And we've been delivering that same food and service ever since. </p>
+				<a href="/ShivaRestaurants/about-Shiva-Restaurants.php" class="waves-effect waves-light btn" style="background: #ee6e73 !important;">Read More &raquo;</a>
+		      </div>
+	</section>
---
+++ ShivaRestaurants/chunks/description.php
@@ -0,0 +1,27 @@
+<section class="fdes">
+		<div class="section white center">
+			<h2 class="header" style="padding:20px; padding-bottom: 30px;">Restaurant Powered By Shiva</h2>
+		      <div class="row container center">
+		        <div id="new"class="col center l8 s12">
+		        	<p style="text-align:left;">
+						<b>DUTIES AND RESPONSIBILITIES:</b>
+					<br>•Friendly and attentive service, including greeting and seating patrons</br>
+					<br>•Taking customized and sometimes detailed orders</br>
+					<br>• Knowing the seasonal menu and being able to promote new items in an engaging way</br>
+					<br>• Anticipating and responding to guests' dining needs</br>
+					<br>• Working closely with other wait staff and kitchen staff</br>
+					<br>• Processing payments</br>
+					<br><b>CLEANING TABLES AND DINING AREA:</b></br>
+					<br>• Opening and closing restaurant as needed</br>
+					<br>• Helping with banquets and events</br>
+					<br>• Other duties as assigned by Chef and Front of House Manager</br>
+					<br>•We would prefer applicants with experience in the following areas</br>
+					<br>•Serving food or working in food-related jobs</br>
+					</p>
+</div>
+		        <div class="col center l4 s12">
+		        	<img height="600" width="auto" style="object-fit: contain;" src="images/17964.jpg" alt="">
+		        </div>
+		        
+		      </div>
+	</section>
